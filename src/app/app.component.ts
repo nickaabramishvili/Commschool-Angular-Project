5d8744012e8c5b561544db338708f261d3910aec
nikabramishvili <n.abramishvili@area.ge>
2022-07-04 17:36:50 +0400

    task 6 homework done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  infodiv = false;
+  onInfoDivClick() {
+    this.infodiv = true;
+    this.infodiv != this.infodiv;
+  }
   title = 'Commschool-Angular-Project';
 }
---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerGridComponent } from './customer-grid/customer-grid.component';
+
+
+
+@NgModule({
+  declarations: [
+    CustomerGridComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerModule { }
