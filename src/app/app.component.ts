4ed4b8bb13f7cea671445142ecd2653253b1a440
nikabramishvili <n.abramishvili@area.ge>
2022-07-13 15:47:32 +0400

    animation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
+import {
+  state,
+  style,
+  trigger,
+  transition,
+  animate,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('componentLoading', [
+      state(
+        'notLoaded',
+        style({
+          width: '0%',
+          opacity: 0,
+          backgroundColor: 'white',
+        })
+      ),
+      state(
+        'loaded',
+        style({
+          width: '100%',
+          opacity: 1,
+          backgroundColor: 'green',
+        })
+      ),
+      transition('notLoaded => loaded', [animate('1s')]),
+      transition('loaded => notLoaded', [animate('1s')]),
+    ]),
+  ],
 })
 export class AppComponent {
   infodiv: boolean = false;
+  animationTriggered: boolean = false;
   onInfoDivClick() {
+    this.animationTriggered = !this.animationTriggered;
     setTimeout(() => {
       this.infodiv = !this.infodiv;
     }, 1000);
   }
+
   title = 'Commschool-Angular-Project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomersModule } from './customers/customers.module';
-import { CustomersDataService } from './services/customers-data.service';
 
 @NgModule({
   declarations: [AppComponent],
