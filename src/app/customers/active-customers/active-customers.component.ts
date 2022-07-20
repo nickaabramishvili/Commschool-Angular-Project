a5b2a94e47a5141c246692105a5782dfef8bd2ae
nikabramishvili <n.abramishvili@area.ge>
2022-07-20 19:55:41 +0400

    prog

--- src/app/customers/active-customers/active-customers.component.ts
+++ src/app/customers/active-customers/active-customers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { CustomersDataService } from '../../services/customers-data.service';
 
 @Component({
@@ -10,17 +10,7 @@ import { CustomersDataService } from '../../services/customers-data.service';
 export class ActiveCustomersComponent {
   constructor(public CustomersDataService: CustomersDataService) {}
 
-  public ColoredText = '';
-  wholeArr = [...this.CustomersDataService.customersData];
-
   displayedColumns = this.CustomersDataService.displayedColumns;
-
-  addCustomerForm = new FormGroup({
-    id: new FormControl('', [Validators.required]),
-    firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('', [Validators.required]),
-    workingYears: new FormControl('', [Validators.required]),
-  });
 
   onDeleteRow(i: number) {
     this.CustomersDataService.deletedCustomersArray = [
@@ -31,44 +21,5 @@ export class ActiveCustomersComponent {
       ...this.CustomersDataService.customersData,
     ];
     console.log(this.CustomersDataService.deletedCustomersArray);
-  }
-
-  onAddUser() {
-    let newUser = {
-      id: this.addCustomerForm.value.id,
-      firstName: this.addCustomerForm.value.firstName,
-      lastName: this.addCustomerForm.value.lastName,
-      workingYears: this.addCustomerForm.value.workingYears,
-    };
-    if (this.addCustomerForm.valid) {
-      this.CustomersDataService.customersData.push(newUser);
-      this.CustomersDataService.customersData = [
-        ...this.CustomersDataService.customersData,
-      ];
-    }
-  }
-
-  onFilterTable() {
-    let nameAndLastName = this.ColoredText.split(' ');
-    let filteredEl;
-
-    if (nameAndLastName.length < 3 && nameAndLastName[0] != '') {
-      for (let i = 0; i < this.CustomersDataService.customersData.length; i++) {
-        if (
-          this.CustomersDataService.customersData[i].firstName.includes(
-            nameAndLastName[0]
-          ) &&
-          this.CustomersDataService.customersData[i].lastName.includes(
-            nameAndLastName[1]
-          )
-        ) {
-          filteredEl = this.CustomersDataService.customersData[i];
-          this.CustomersDataService.customersData = [];
-          this.CustomersDataService.customersData.push(filteredEl);
-        }
-      }
-    } else if (this.ColoredText == '') {
-      this.CustomersDataService.customersData = [...this.wholeArr];
-    }
   }
 }
--- src/app/customers/customers-wrapper/customers-wrapper.component.ts
+++ src/app/customers/customers-wrapper/customers-wrapper.component.ts
@@ -6,7 +6,8 @@ import {
   transition,
   animate,
 } from '@angular/animations';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomersDataService } from '../../services/customers-data.service';
 @Component({
   selector: 'app-customers-wrapper',
   templateUrl: './customers-wrapper.component.html',
@@ -17,8 +18,8 @@ import {
         'notLoaded',
         style({
           width: '0%',
-          opacity: 0,
-          backgroundColor: 'white',
+          opacity: 0.1,
+          backgroundColor: 'blue',
         })
       ),
       state(
@@ -37,13 +38,63 @@ import {
 export class CustomersWrapperComponent implements OnInit {
   infodiv: boolean = false;
   animationTriggered: boolean = false;
+  addCustomerForm: FormGroup;
+  public ColoredText = '';
+  wholeArr = [...this.CustomersDataService.customersData];
+  constructor(
+    @Inject('VERSION-NUMBER') public versionNumber: string,
+    public CustomersDataService: CustomersDataService
+  ) {
+    this.addCustomerForm = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      workingYears: new FormControl('', [Validators.required]),
+    });
+  }
   onInfoDivClick() {
     this.animationTriggered = !this.animationTriggered;
     setTimeout(() => {
       this.infodiv = !this.infodiv;
     }, 1000);
   }
-  constructor(@Inject('VERSION-NUMBER') public versionNumber: string) {}
+  onAddUser() {
+    let newUser = {
+      id: this.addCustomerForm.value.id,
+      firstName: this.addCustomerForm.value.firstName,
+      lastName: this.addCustomerForm.value.lastName,
+      workingYears: this.addCustomerForm.value.workingYears,
+    };
+    if (this.addCustomerForm.valid) {
+      this.CustomersDataService.customersData.push(newUser);
+      this.CustomersDataService.customersData = [
+        ...this.CustomersDataService.customersData,
+      ];
+    }
+  }
+  onFilterTable() {
+    let nameAndLastName = this.ColoredText.split(' ');
+    let filteredEl;
+
+    if (nameAndLastName.length < 3 && nameAndLastName[0] != '') {
+      for (let i = 0; i < this.CustomersDataService.customersData.length; i++) {
+        if (
+          this.CustomersDataService.customersData[i].firstName.includes(
+            nameAndLastName[0]
+          ) &&
+          this.CustomersDataService.customersData[i].lastName.includes(
+            nameAndLastName[1]
+          )
+        ) {
+          filteredEl = this.CustomersDataService.customersData[i];
+          this.CustomersDataService.customersData = [];
+          this.CustomersDataService.customersData.push(filteredEl);
+        }
+      }
+    } else if (this.ColoredText == '') {
+      this.CustomersDataService.customersData = [...this.wholeArr];
+    }
+  }
 
   ngOnInit(): void {}
 }
